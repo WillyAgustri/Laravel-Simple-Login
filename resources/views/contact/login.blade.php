84ce32fcfeca6fbca91de84c0c03de06f280f715
WillyAgustri <willyofficial082@gmail.com>
2023-06-04 02:52:35 +0700

    fix name in input form

--- resources/views/contact/login.blade.php
+++ resources/views/contact/login.blade.php
@@ -6,18 +6,20 @@
 
             <h1>Masuk Akun</h1>
 
-            <form action="">
+            <form action="{{ route('check') }}" method="POST" name="check">
+                {{ method_field('post') }}
                 @csrf
+
                 <div class="input-email">
                     <br>Email Address</br>
-                    <input class="input-field glass-effect" type="text" placeholder="Masukan Email">
+                    <input class="input-field glass-effect" name="email" type="email" placeholder="Masukan Email">
                 </div>
                 <div class="input-password ">
                     <br>Password</br>
-                    <input class="input-field glass-effect" type="text" placeholder="Masukan Password">
+                    <input class="input-field glass-effect" name="password" type="password" placeholder="Masukan Password">
                 </div>
                 <div class="login-button d-flex justify-content-center mt-3 mb-2">
-                    <input name="submit" id="" class="submit btn btn-sm" type="button" value="Login">
+                    <input name="submit" class="submit btn btn-sm" type="submit" value="Login">
                 </div>
                 <div>
                     Atau
