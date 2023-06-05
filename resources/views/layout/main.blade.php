267c628bdc75d9b0c9a5dbf034ce6066826d7400
WillyAgustri <willyofficial082@gmail.com>
2023-06-06 04:28:56 +0700

    fix responsive landing

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -26,22 +26,25 @@
 </header>
 
 <body>
-    <div class="container ">
+    <div class="container my-1">
         @yield('content')
     </div>
 
-    <div class="glass-effect d-flex flex-column justify-content-center flex-md-row text-center py-3 ">
-        <!-- Copyright -->
-        <div class="text-white text-center py-2">
-            Copyright © 2023. Willy Agustri Djabar All rights reserved.
-        </div>
-        <!-- Copyright -->
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
 </body>
+<footer class="glass-effect d-flex flex-column justify-content-center flex-md-row text-center py-3 container-fluid">
+    <div class="containers">
+        <div class="row">
+            <div class="col text-white">
+                <p class="m-0">Copyright © 2023. Willy Agustri Djabar All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</footer>
+
 
 </html>
